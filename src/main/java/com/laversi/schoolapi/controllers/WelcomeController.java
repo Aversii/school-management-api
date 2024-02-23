1192f6bab81e8d5286c24c747c7cf1b62e3e6855
aversii <lucasaversi@gmail.com>
2024-02-23 00:23:20 -0300

    feat: Add welcome controller class and initial setup with Lombok and Spring DevTools

---
+++ src/main/java/com/laversi/schoolapi/controllers/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.laversi.schoolapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "<div style=\"text-align: center; margin-top: 50px;\">" +
+                "<h1>Welcome to API</h1>" +
+                "</div>";
+    }
+
+}
